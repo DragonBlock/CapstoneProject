b3f7c2c00d1994d2d62e591d08f2c85378cafdda
JustDamion <shirkeydamion@gmail.com>
2022-04-10 20:15:58 -0500

    Images Added, Menu Bar Reformatted

--- Checkout.aspx.designer.cs
+++ Checkout.aspx.designer.cs
@@ -67,5 +67,14 @@ namespace CapstoneProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtCountry;
+
+        /// <summary>
+        /// btnCheckout control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCheckout;
     }
 }
---
+++ SeeCart.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CapstoneProject
+{
+    public partial class MenuSides : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMeals_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Menu-Meals.aspx");
+        }
+
+        protected void btnDrinks_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Menu-Drinks.aspx");
+        }
+    }
+}
