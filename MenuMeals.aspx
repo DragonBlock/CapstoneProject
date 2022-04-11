3c2dea4b8645056f8c769ed7b0f4c340f39aba5e
DragonBlock <michaeljacob2000@hotmail.com>
2022-04-10 21:42:19 -0400

    Menu Stuff

--- MenuMeals.aspx
+++ MenuMeals.aspx
@@ -1,16 +1,35 @@
 ﻿<%@ Page Title="MenuMeals" Async="true" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MenuMeals.aspx.cs" Inherits="CapstoneProject.MenuMeals" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        .MenuItem 
+        { 
+            width: 10%; height: 20%; float: left;
+        }
+        .MenuImage 
+        {
+            height: 16%; width: 10%;
+        }
+        .MenuLabel 
+        {
+            text-align: center;
+            width: 10%;
+            height: 20%;
+        }
+        .MenuButton 
+        {
+            width: 10%;
+            height: 20%;
+        }
+    </style>
     <div class="navbar">
         <asp:Button ID="btnMeals" Text="Meals" runat="server" />
         <asp:Button ID="btnSides" Text="Sides" runat="server" />
         <asp:Button ID="btnDrinks" Text="Drinks" runat="server" />
     </div>
-    <div class="content">
-        <figure class="menuItem" style="padding: 15px;">
-            <img alt="" src="" style="height: 15%; width: 10%;" />
-            <figcaption style="text-align: center; width: 10%;">Name</figcaption>
-            <input id="btnAddItem" type="button" value="Add To Order" style="width: 10%;" />
-        </figure>
+    <div class="content" >
+        <asp:Panel ID="MainPanel" style="width:100%; height:80%;" runat="server">
+
+        </asp:Panel>
     </div>
 </asp:Content>
--- MenuMeals.aspx.cs
+++ MenuMeals.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,7 +11,52 @@ namespace CapstoneProject
 {
     public partial class MenuMeals : Page
     {
+        DatabaseAccessor accessor;
         protected void Page_Load(object sender, EventArgs e)
+        {
+            accessor = new DatabaseAccessor();
+            accessor.ConnectToDatabase(Server.MapPath("/") + "FoodOrderingDB.mdb");
+
+            List<Panel> panelsList = new List<Panel>();
+            DataTable foodTable = accessor.GetItemList(ItemType.Main);
+            foreach (DataRow foodRow in foodTable.Rows)
+            {
+                Panel p = new Panel();
+                p.ID = "pan" + foodRow.Field<int>("Item_ID");
+                p.CssClass = "MenuItem";
+
+                Image i = new Image();
+                i.ID = "img" + foodRow.Field<int>("Item_ID");
+                i.ImageUrl = Server.MapPath("/Images/") + foodRow.Field<string>("Item_Image");
+                i.ImageAlign = ImageAlign.Middle;
+                i.CssClass = "MenuImage";
+                Label l = new Label();
+                l.ID = "lbl" + foodRow.Field<int>("Item_ID");
+                l.CssClass = "MenuLabel";
+                l.Text = foodRow.Field<string>("Item_Name") + " $" + foodRow.Field<decimal>("Item_Price");
+
+                Button b = new Button();
+                b.Click += Item_Click;
+                b.ID = "btn" + foodRow.Field<int>("Item_ID");
+                b.CssClass = "MenuButton";
+                b.Text = "Add To Order";
+                
+
+                p.Controls.Add(i);
+                p.Controls.Add(l);
+                p.Controls.Add(b);
+
+                lock (Controls.SyncRoot)
+                {
+                    MainPanel.Controls.Add(p);
+                }
+
+                panelsList.Add(p);
+            }
+
+        }
+
+        protected void Item_Click(object sender, EventArgs e)
         {
 
         }
